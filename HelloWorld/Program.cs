466b55df7bbcb1b578b56423646a3f20d20872a4
Hoàng Hữu Tiến Đạt <dathht.21ad@vku.udn.vn>
2023-05-23 19:54:26 +0700

    Create Program.cs

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c1= new MyClass();
+            Console.WriteLine($"HelloWord!{c1.ReturnMessage()}");
+        }
+    }
+}
+
