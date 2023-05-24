4f0eb310d2863c91f277107467a0d43eb875c415
Hoàng Hữu Tiến Đạt <dathht.21ad@vku.udn.vn>
2023-05-25 00:05:27 +0700

    ex5

---
+++ BT5/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Ex5-nhập vào ngày tháng năm sinh -> in ra số ngày cần để tới sinh nhật tiếp theo
+using System;
+namespace Ex5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap ngay thang nam sinh: ");
+            int day = int.Parse(Console.ReadLine());
+            int month = int.Parse(Console.ReadLine());
+            int year = int.Parse(Console.ReadLine());
+            DateTime birthday = new DateTime(year, month, day);
+            DateTime today = DateTime.Today;
+            DateTime nextBirthday = new DateTime(today.Year, birthday.Month, birthday.Day);
+            if (nextBirthday < today)
+            {
+                nextBirthday = nextBirthday.AddYears(1);
+            }
+            int daysUntilBirthday = (nextBirthday - today).Days;
+            Console.WriteLine("So ngay can de toi sinh nhat tiep theo la: " + daysUntilBirthday);
+            
+        }
+    }
+}
