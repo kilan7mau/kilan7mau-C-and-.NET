6c8bb8c39386b44f73183f6f429046fd3511b693
Hoàng Hữu Tiến Đạt <dathht.21ad@vku.udn.vn>
2023-05-24 23:48:24 +0700

    Ex4

---
+++ BT4/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Ex4-Ứng dụng chuyển đổi tiền tệ
+using System;
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double rate = 23000;
+            Console.WriteLine("Nhap so tien USD: ");
+            double usd = double.Parse(Console.ReadLine());
+            double vnd = usd * rate;
+            Console.WriteLine("So tien VND: " + vnd);
+        }
+    }
+}
