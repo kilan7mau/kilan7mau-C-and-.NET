b993318cd0d6b089b4183d12ce6ba587420e61ca
Hoàng Hữu Tiến Đạt <dathht.21ad@vku.udn.vn>
2023-05-24 23:31:19 +0700

    Ex1

---
+++ BT2/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//BT2-Sử dụng toán tử
+using System;
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float width, height;
+            Console.Write("Enter width: ");
+            width = float.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            height = float.Parse(Console.ReadLine());
+            float area = width * height;
+            Console.WriteLine("Area is: " + area);
+            
+            
+        }
+    }
+}
