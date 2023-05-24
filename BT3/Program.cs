3cb640cbc6cbe683532fb772a1ef0ea009e20d07
Hoàng Hữu Tiến Đạt <dathht.21ad@vku.udn.vn>
2023-05-24 23:36:10 +0700

    Ex3

---
+++ BT3/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Ex3-Hiển thị lời chào
+using System;
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Hello: " + yourName);
+            Console.WriteLine($"Hello: {yourName}");
+        }
+    }   
+}
