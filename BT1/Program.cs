b01deb879f97a3d6ad23118f78ef8d327d4e5cdb
Hoàng Hữu Tiến Đạt <dathht.21ad@vku.udn.vn>
2023-05-24 23:22:23 +0700

    Ex1

---
+++ BT1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+// BT1_Hiển thị thời gian hệ thống
+using System;
+namespace Ex1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            System.Console.WriteLine("Datetime Now is :" + localDate);
+            Console.WriteLine($"Datetime Now is : {localDate}");
+        }
+    }
+}
